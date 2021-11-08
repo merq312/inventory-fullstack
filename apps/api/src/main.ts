92472a9c15255fd7234dd09b5b91df928e247e04
Chamila Gunasena <merc312@gmail.com>
2021-11-08 10:45:35 -0500

    serve app from api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
+import * as path from 'path'
 import { Message } from '@inventory-fullstack/api-interfaces';
 
 const app = express();
+const CLIENT_BUILD_PATH = path.join(__dirname, '../my-app');
 
 const greeting: Message = { message: 'Welcome to api!' };
 
@@ -9,8 +11,12 @@ app.get('/api', (req, res) => {
   res.send(greeting);
 });
 
-const port = process.env.port || 3333;
+app.get('*', (req, res) => {
+  res.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+});
+
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
+  console.log('Listening at http://localhost:' + port);
 });
 server.on('error', console.error);
