d91bc1bf52ae886e72a6360af36938742ef1c06c
merq312 <60433479+merq312@users.noreply.github.com>
2021-11-22 23:32:11 -0500

    feat: setup express

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,58 @@
 import * as express from 'express';
 import * as path from 'path';
-import { Message } from '@inventory-fullstack/api-interfaces';
+import * as cors from 'cors';
+import * as morgan from 'morgan'
+import * as xss from 'xss-clean'
+import * as compression from 'compression'
+import * as cookieParser from 'cookie-parser';
+import * as rateLimit from 'express-rate-limit';
+import productRouter from './routes/productRouter'
 
 const CLIENT_BUILD_PATH = path.join(__dirname, '../app');
 
 const app = express();
+app.enable('trust proxy')
+
 app.use(express.static(CLIENT_BUILD_PATH));
 
-const greeting: Message = { message: 'Hello from api!' };
+if (process.env.NODE_ENV === 'development') {
+  const origin = 'http://localhost:3333'
+  app.use(cors({ credentials: true, origin }))
+  app.options(origin, cors({ credentials: true, origin }))
+} else {
+  const origin = 'https://mighty-atoll-05391.herokuapp.com/'
+  app.use(cors({ credentials: true, origin }))
+  app.options(origin, cors({ credentials: true, origin }))
+}
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again later',
+})
+
+app.use('/api', limiter)
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+)
+app.use(express.urlencoded({ extended: true, limit: '10kb' }))
+app.use(cookieParser())
+app.use(xss())
+app.use(compression())
+
+app.use('/api/v1/product', productRouter)
+// app.use('/api/v1/menu')
+// app.use('/api/v1/store')
+// app.use('/api/v1/user')
 
 app.get('/api', (req, res) => {
-  res.send(greeting);
+  res.send({ message: "Hello from api"});
 });
 
 app.get('*', (req, res) => {
---
+++ apps/api/src/routes/productRouter.ts
@@ -0,0 +1,8 @@
+import * as express from 'express'
+import { checkHealth } from '../controllers/productController';
+
+const router = express.Router()
+
+router.get('/', checkHealth)
+
+export default router
