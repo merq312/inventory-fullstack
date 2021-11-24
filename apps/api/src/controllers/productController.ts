faa56ed43a56b21f27cd6c88bd52c1ec4510dda4
merq312 <60433479+merq312@users.noreply.github.com>
2021-11-24 18:55:39 -0500

    feat: implement get all product counts route

--- apps/api/src/controllers/productController.ts
+++ apps/api/src/controllers/productController.ts
@@ -1,58 +1,78 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export const getProductCount = async (req, res, next) => {
-  const { store_name, product_name, date } = req.params;
-
-  let day = '';
+function getDate(date) {
   if (date) {
-    try {
-      day = new Date(date).toISOString()
-    } catch (err) {
-      return next()
-    }
+    return new Date(date).toISOString();
   } else {
-    day = new Date().toISOString()
+    return new Date().toISOString();
   }
+}
 
-  const store = await prisma.store.findUnique({
+async function findStore(store_name) {
+  return await prisma.store.findUnique({
     where: { name: store_name }
   });
-  const menuItem = await prisma.menuItem.findUnique({
-    where: { name: product_name }
+}
+
+async function findProductCount(menuItemOnStoreId: number, day: string) {
+  return await prisma.productCount.findFirst({
+    where: {
+      menuItemOnStoreId: menuItemOnStoreId,
+      day: day
+    }
   });
+}
 
-  if (store && menuItem) {
-    const menuItemOnStore =
-      await prisma.menuItemsOnStores.findFirst({
-        where: {
-          menuItemId: menuItem.id,
-          storeId: store.id
-        }
-      });
+async function findAllMenuItemsOnStore(storeId: number) {
+  return await prisma.menuItemsOnStores.findMany({
+    where: {
+      storeId: storeId
+    }
+  });
+}
 
-    let menuItemOnStoreCount = await prisma.productCount.findFirst({
-      where: {
-        menuItemOnStoreId: menuItemOnStore.id,
-        day: day
-      }
-    });
+async function createProductCountEntry(menuItemOnStoreId: number) {
+  return await prisma.productCount.create({
+    data: {
+      menuItemOnStoreId: menuItemOnStoreId
+    }
+  });
+}
 
-    if (!menuItemOnStoreCount && !date) {
+export const getAllProductsCount = async (req, res, next) => {
+  const { store_name, date } = req.params;
+
+  let day = '';
+  try {
+    day = getDate(date);
+  } catch (err) {
+    return next();
+  }
+
+  const store = await findStore(store_name);
+
+  if (store) {
+    const menuItemsOnStore = await findAllMenuItemsOnStore(store.id);
+
+    const productCounts = await Promise.all(menuItemsOnStore.map(async (item) => {
+      let productCount = await findProductCount(item.id, day);
+
       // If a product count doesn't exist for the current day and no date is specified (ie- user is trying to get product count for today), make an entry for it
-      menuItemOnStoreCount = await prisma.productCount.create({
-        data: {
-          menuItemOnStoreId: menuItemOnStore.id
-        }
+      if (!productCount && !date) {
+        productCount = await createProductCountEntry(item.id);
+      }
+
+      return productCount;
+    }));
+
+    if (productCounts[0]) {
+      return res.status(200).json({
+        status: 'success',
+        data: productCounts
       });
     }
-
-    return res.status(200).json({
-      status: 'success',
-      data: menuItemOnStoreCount
-    });
   }
-  // If either store or menuItem are not found, throw error
   next();
 };
--- apps/api/src/routes/productRouter.ts
+++ apps/api/src/routes/productRouter.ts
@@ -1,13 +1,11 @@
 import * as express from 'express';
 import { checkHealth } from '../controllers/miscController';
-import { getProductCount } from '../controllers/productController';
+import { getAllProductsCount} from '../controllers/productController';
 
 const router = express.Router();
 
-// router.get('/:store_name', getProductCount);
-// router.get('/:store_name/:date', getProductCount);
-router.get('/:store_name/:product_name', getProductCount);
-router.get('/:store_name/:product_name/:date', getProductCount);
+router.get('/:store_name', getAllProductsCount);
+router.get('/:store_name/:date', getAllProductsCount);
 
 router.get('/check_health', checkHealth);
 
