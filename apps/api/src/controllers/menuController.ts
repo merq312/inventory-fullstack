556d096c4117085730df5fe66e67e55227135769
merq312 <60433479+merq312@users.noreply.github.com>
2021-11-24 19:52:51 -0500

    feat: implement menu api routes

---
+++ apps/api/src/controllers/menuController.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function createMenuItem(name) {
+  return await prisma.menuItem.create({
+    data: {
+      name: name
+    }
+  });
+}
+
+export async function addMenuItem(req, res, next) {
+  const { name } = req.params;
+  const [menuItem] = await Promise.all([createMenuItem(name)
+    .then(menuItem => menuItem)
+    .catch(() => null)]);
+
+  return menuItem
+    ? res.status(200).json({
+      status: 'success',
+      data: menuItem
+    })
+    : next();
+}
+
+async function updateMenuItemName(oldName, newName) {
+  return await prisma.menuItem.update({
+    where: {
+      name: oldName
+    },
+    data: {
+      name: newName
+    }
+  });
+}
+
+export async function editMenuItemName(req, res, next) {
+  const { oldName, newName } = req.params;
+  const [menuItem] = await Promise.all([updateMenuItemName(oldName, newName)
+    .then(menuItem => menuItem)
+    .catch(() => null)]);
+
+  return menuItem
+    ? res.status(200).json({
+      status: 'success',
+      data: menuItem
+    })
+    : next();
+}
--- apps/api/src/routes/menuRouter.ts
+++ apps/api/src/routes/menuRouter.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
 import { checkHealth } from '../controllers/miscController';
+import { addMenuItem, editMenuItemName } from '../controllers/menuController';
 
 const router = express.Router();
 
-router.get('/', checkHealth);
+router.post('/:name', addMenuItem)
+router.post('/:oldName/:newName', editMenuItemName)
+router.get('/check_health', checkHealth);
 
 export default router;
