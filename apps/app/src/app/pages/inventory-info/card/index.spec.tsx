f6f261259ddb4d8df8eceb3cf9bc7821d4566d42
merq312 <60433479+merq312@users.noreply.github.com>
2021-11-30 14:17:20 -0500

    test: add required props to components

--- apps/app/src/app/pages/inventory-info/card/index.spec.tsx
+++ apps/app/src/app/pages/inventory-info/card/index.spec.tsx
@@ -1,10 +1,22 @@
 import { render } from '@testing-library/react';
 
-import InventoryInfo from './index';
+import InventoryInfoCard from './index';
 
 describe('InventoryInfo', () => {
+  const item = {
+    id: 1,
+    day: '2021-11-30',
+    overnightCount: 0,
+    morningCount: 0,
+    afternoonCount: 0,
+    leftoverCountOne: 0,
+    leftoverCountTwo: 0,
+    menuItemOnStoreId: 1,
+    name: 'california'
+  };
+
   it('should render successfully', () => {
-    const { baseElement } = render(<InventoryInfo />);
+    const { baseElement } = render(<InventoryInfoCard item={item} />);
     expect(baseElement).toBeTruthy();
   });
 });
