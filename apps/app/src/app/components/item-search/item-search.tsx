5d6e651c424d2ce48985f776b2bf0a24d7d2c57f
merq312 <60433479+merq312@users.noreply.github.com>
2021-12-01 14:25:14 -0500

    add change event handler

--- apps/app/src/app/components/item-search/item-search.tsx
+++ apps/app/src/app/components/item-search/item-search.tsx
@@ -1,12 +1,18 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import { SyntheticEvent } from 'react';
 
 function ItemSearch() {
+  const handleChange = (event: SyntheticEvent, value: string | null) => {
+    console.log(value)
+  }
+
   return (
     <Autocomplete
       disablePortal
       id='combo-box-demo'
       options={['California', 'Salmon Avo']}
+      onChange={handleChange}
       renderInput={(params) => <TextField {...params} label='Search' />}
     />
   );
