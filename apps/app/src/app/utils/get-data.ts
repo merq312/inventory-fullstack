980feb9e27bd3342dece6ef81b9eafaf53806ee0
merq312 <60433479+merq312@users.noreply.github.com>
2021-12-03 14:54:22 -0500

    feat: implement API requests

--- apps/app/src/app/utils/get-data.ts
+++ apps/app/src/app/utils/get-data.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
+import { PostItem } from '../pages/inventory-input';
+
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333'
+    : 'https://mighty-atoll-05391.herokuapp.com';
 
 export async function getProductData(date: string) {
   try {
-    const baseUrl =
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3333'
-        : 'https://mighty-atoll-05391.herokuapp.com';
     const req = await axios.get(`${baseUrl}/api/v1/product/rcss/${date}`);
     return req.data.data;
   } catch (error) {
@@ -15,10 +17,6 @@ export async function getProductData(date: string) {
 
 export async function getAllStores() {
   try {
-    const baseUrl =
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3333'
-        : 'https://mighty-atoll-05391.herokuapp.com';
     const req = await axios.get(`${baseUrl}/api/v1/store/all-stores`);
     return req.data.data;
   } catch (error) {
@@ -28,12 +26,29 @@ export async function getAllStores() {
 
 export async function getAllMenuItems() {
   try {
-    const baseUrl =
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3333'
-        : 'https://mighty-atoll-05391.herokuapp.com';
     const req = await axios.get(`${baseUrl}/api/v1/menu/all-menu-items`);
     return req.data.data;
+  } catch (error) {
+    throw new Error('Server error');
+  }
+}
+
+export async function createNewMenuItem(newItemName: string) {
+  try {
+    const req = await axios.post(`${baseUrl}/api/v1/menu`, {
+      name: newItemName,
+    });
+    return req.data.data;
+  } catch (error) {
+    throw new Error('Server error');
+  }
+}
+
+export async function updateProductCounts(post: Array<PostItem>, date: string) {
+  try {
+    await axios.patch(`${baseUrl}/api/v1/product/rcss/${date}`, {
+      productData: post,
+    });
   } catch (error) {
     throw new Error('Server error');
   }
