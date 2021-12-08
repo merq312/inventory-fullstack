15ab5006cfb5b498d2bfd49ac7159995b5f2365d
merq312 <merc312@gmail.com>
2021-12-07 19:11:38 -0500

    feat: frontend menu item rename functionality

--- apps/app/src/app/pages/dashboard/menu-table/index.tsx
+++ apps/app/src/app/pages/dashboard/menu-table/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
@@ -12,6 +12,7 @@ import { MenuItemData } from '../index';
 import InputTableRow from '../../../components/input-table-row/input-table-row';
 import AddIcon from '@mui/icons-material/Add';
 import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
 
 type AppProps = {
   menuData: Array<MenuItemData>;
@@ -32,7 +33,100 @@ function MenuTable({
   setNewItemError,
   errorMsg,
 }: AppProps) {
-  const [showInput, setShowInput] = useState(false);
+  const [showNewItemInput, setShowNewItemInput] = useState(false);
+  const [showRenameButton, setShowRenameButton] = useState('');
+  const [renameInput, setRenameInput] = useState('');
+  const [renameValue, setRenameValue] = useState('');
+
+  useEffect(() => {
+    console.log(renameValue);
+  }, [renameValue]);
+
+  function mapMenuData() {
+    return menuData.map((item) =>
+      renameInput === item.name ? (
+        <InputTableRow
+          key={item.name}
+          placeholder={item.name}
+          error={false}
+          close={() => {
+            setRenameInput('');
+          }}
+          dispatch={setRenameValue}
+        />
+      ) : (
+        <TableRow
+          key={item.name}
+          sx={{
+            '&:last-child td, &:last-child th': { border: 0 },
+            '&:hover': { backgroundColor: '#e3f2fd' },
+            backgroundColor: () => (item.inStore ? '#eeeeee' : 'white'),
+          }}
+          onMouseOver={() => setShowRenameButton(item.name)}
+        >
+          <TableCell
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+            component="th"
+            scope="row"
+          >
+            <span>{item.name}</span>
+            <Box>
+              {!renameInput && showRenameButton === item.name && (
+                <IconButton
+                  size="small"
+                  onClick={() => setRenameInput(item.name)}
+                >
+                  <EditIcon fontSize="small" />
+                </IconButton>
+              )}
+              {!item.inStore && selectedStore && (
+                <IconButton
+                  size="small"
+                  onClick={() => setNewStoreItemName(item.name)}
+                >
+                  <AddIcon fontSize="small" />
+                </IconButton>
+              )}
+            </Box>
+          </TableCell>
+        </TableRow>
+      )
+    );
+  }
+
+  function displayStatusMessage() {
+    return (
+      <TableRow
+        sx={{
+          '&:last-child td, &:last-child th': { border: 0 },
+          '&:hover': { backgroundColor: '#e3f2fd' },
+        }}
+      >
+        <TableCell component="th" scope="row">
+          {errorMsg ? errorMsg : 'Loading...'}
+        </TableCell>
+        <TableCell align="right">{''}</TableCell>
+      </TableRow>
+    );
+  }
+
+  function displayNewItemInput() {
+    return (
+      <InputTableRow
+        error={newItemError}
+        placeholder="Menu Item Name"
+        close={() => {
+          setShowNewItemInput(false);
+          setNewItemError(false);
+        }}
+        dispatch={setNewMenuItemName}
+      />
+    );
+  }
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
@@ -43,66 +137,16 @@ function MenuTable({
               <TableCell>Menu Items</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
-            {menuData.length !== 0 ? (
-              menuData.map((item) => (
-                <TableRow
-                  key={item.name}
-                  sx={{
-                    '&:last-child td, &:last-child th': { border: 0 },
-                    '&:hover': { backgroundColor: '#e3f2fd' },
-                    backgroundColor: () => (item.inStore ? '#eeeeee' : 'white'),
-                  }}
-                >
-                  <TableCell
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'space-between',
-                      alignItems: 'center',
-                    }}
-                    component="th"
-                    scope="row"
-                  >
-                    <span>{item.name}</span>
-                    {!item.inStore && selectedStore && (
-                      <IconButton
-                        size="small"
-                        onClick={() => setNewStoreItemName(item.name)}
-                      >
-                        <AddIcon />
-                      </IconButton>
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow
-                sx={{
-                  '&:last-child td, &:last-child th': { border: 0 },
-                  '&:hover': { backgroundColor: '#e3f2fd' },
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  {errorMsg ? errorMsg : 'Loading...'}
-                </TableCell>
-                <TableCell align="right">{''}</TableCell>
-              </TableRow>
-            )}
-            {showInput && (
-              <InputTableRow
-                error={newItemError}
-                placeholder="Menu Item Name"
-                close={() => {
-                  setShowInput(false);
-                  setNewItemError(false);
-                }}
-                dispatch={setNewMenuItemName}
-              />
-            )}
+          <TableBody onMouseLeave={() => setShowRenameButton('')}>
+            {menuData.length !== 0 ? mapMenuData() : displayStatusMessage()}
+            {showNewItemInput && displayNewItemInput()}
           </TableBody>
         </Table>
       </TableContainer>
-      <Button sx={{ alignSelf: 'end' }} onClick={() => setShowInput(true)}>
+      <Button
+        sx={{ alignSelf: 'end' }}
+        onClick={() => setShowNewItemInput(true)}
+      >
         Add new menu item
       </Button>
     </Box>
