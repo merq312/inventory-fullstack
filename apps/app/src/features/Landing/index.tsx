e408d844fe70a03c459e24101f83a674b47bcbe1
merq312 <merc312@gmail.com>
2022-01-12 01:10:33 -0500

    feat: add links to landing page

--- apps/app/src/features/Landing/index.tsx
+++ apps/app/src/features/Landing/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Paper, Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
@@ -8,6 +9,8 @@ const Landing = () => {
 
   const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
     useAuth0();
+
+  const theme = useTheme();
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -58,25 +61,55 @@ const Landing = () => {
           Bento Sushi Product Tracker
         </Typography>
       </Box>
-      <Typography variant="body1" component="div" gutterBottom>
-        {m.message}
-      </Typography>
-      {isLoading ? (
-        <Typography variant="body1" component="div" gutterBottom>
-          Loading ...
-        </Typography>
-      ) : isAuthenticated ? (
-        <>
-          <Typography variant="h5" component="h2" gutterBottom>
-            {user ? user.name : ''}
-          </Typography>
+
+      <Paper
+        sx={{
+          display: 'flex',
+          mb: 3,
+          '& > *': {
+            p: 2,
+            m: 1,
+            color: theme.palette.primary.main,
+          },
+        }}
+      >
+        <Link to="Dashboard">Dashboard</Link>
+        <Link to="inventory-input">Inventory Input</Link>
+        <Link to="inventory-info">Inventory Information</Link>
+      </Paper>
+
+      <Paper
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          '& > *': {
+            py: 2,
+            px: 4,
+            m: 1,
+          },
+        }}
+      >
+        {/*<Typography variant="body1" component="div" gutterBottom>*/}
+        {/*  {m.message}*/}
+        {/*</Typography>*/}
+        {isLoading ? (
           <Typography variant="body1" component="div" gutterBottom>
-            {user ? user.email : ''}
+            Loading ...
           </Typography>
-        </>
-      ) : (
-        <p>Not logged in</p>
-      )}
+        ) : isAuthenticated ? (
+          <>
+            <Typography variant="h5" component="h2" gutterBottom>
+              {user ? user.name : ''}
+            </Typography>
+            <Typography variant="body1" component="div" gutterBottom>
+              {user ? user.email : ''}
+            </Typography>
+          </>
+        ) : (
+          <p>You are not logged in</p>
+        )}
+      </Paper>
     </Box>
   );
 };
